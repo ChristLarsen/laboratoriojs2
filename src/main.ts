8c739a0bd37845e5b8fae3a85bc6baf607553152
ChristLarsen <c.larsen@mayoral.com>
2024-10-27 18:02:01 +0100

    Cambios en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
-import "./style.css";
+console.log("Cena. Somos 6 amigos");
+console.log("Total de la cena-> 120 €");
+console.log("Importe de bebidas-> 18 €");
 
-console.log("Hello Typescript!");
+// prueba
+console.log("Importe a pagar por la cena (las bebidas las pago yo)->" + (120-18));
+console.log("Pues cada uno tiene que pagar->" + ((120-18)/6));
+console.log("menos yo, que pago además los 18€ de las bebidas->" + (((120-18)/6)+18));
